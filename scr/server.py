cebb84a2ec79f5d599e13df25c33e30de6c7856f
Marco <marcoscherillo98@gmail.com>
2026-04-12 03:22:41 -0400

    Set up Header, method and body extractions also added a responce

--- scr/server.py
+++ scr/server.py
@@ -14,5 +14,36 @@ print("Client connected", address)
 
 #set up get request
 data = client.recv(1024)
-print(data)
-print(data.decode())
+
+request = data.decode()
+
+lines = request.split("\r\n")
+for line in lines:
+    print(line)
+
+request = lines[0].split()
+
+method = request[0]
+path = request[1]
+version = request[2]
+
+headers = {}
+
+for i in range(1, len(lines)):
+    line = lines[i].split(":")
+    if len(line) > 1:
+        headers[line[0]] = line[1]
+
+print(headers)
+
+body = "Hello World"
+
+response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: Text/plain\r\n"
+    f"content-Lenght: {len(body)}\r\n"
+    "\r\n"
+    f"{body}"
+)
+
+client.sendall(response.encode())
