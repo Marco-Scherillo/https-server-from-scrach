0138116ace05e5abfeb09c2adc9981d500afbda1
Marco <marcoscherillo98@gmail.com>
2026-04-13 04:01:09 -0400

    changed format also parse headers and body seperatelly

--- scr/server.py
+++ scr/server.py
@@ -1,49 +1,52 @@
 import socket
 import sys
 
-
+#Start server
 server = socket.socket()
-
 server.bind(("localhost", 8080))
-
 server.listen()
 print("Server is listening...")
 
+#unpack client data 
 client, address = server.accept()
 print("Client connected", address)
 
 #set up get request
 data = client.recv(1024)
-
 request = data.decode()
 
-lines = request.split("\r\n")
-for line in lines:
-    print(line)
+lines, body = request.split("\r\n\r\n")
 
-request = lines[0].split()
-
-method = request[0]
-path = request[1]
-version = request[2]
+lines = lines.split("\r\n")
+method, path, version = lines[0].split(" ")
 
 headers = {}
+for line in lines[1:]:
+    if line == " ":
+        break
+    key, value = line.split(":", 1)
+    headers[key] = value
 
-for i in range(1, len(lines)):
-    line = lines[i].split(":")
-    if len(line) > 1:
-        headers[line[0]] = line[1]
-
+print(method)
 print(headers)
+print(body)
 
-body = "Hello World"
+
+
+
+#Responce
+responce_body = "Hello Julian"
 
 response = (
     "HTTP/1.1 200 OK\r\n"
     "Content-Type: Text/plain\r\n"
-    f"content-Lenght: {len(body)}\r\n"
+    f"content-Lenght: {len(responce_body)}\r\n"
     "\r\n"
-    f"{body}"
+    f"{responce_body}"
 )
 
+
+#Close connection
 client.sendall(response.encode())
+client.close()
+
