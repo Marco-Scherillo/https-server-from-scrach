fdef6645d2d8cc8aea08b8821468dba81a93da3b
Marco <marcoscherillo98@gmail.com>
2026-04-23 14:47:28 -0400

    refactored client handling and request parceing to functions futher refactoring is needed

--- scr/server.py
+++ scr/server.py
@@ -1,5 +1,50 @@
 import socket
 import sys
+import threading
+
+def parce_request(data):
+    request = data.decode()
+
+    lines, body = request.split("\r\n\r\n")
+
+    lines = lines.split("\r\n")
+    method, path, version = lines[0].split(" ")
+
+    headers = {}
+    for line in lines[1:]:
+        if line == " ":
+            break
+        key, value = line.split(":", 1)
+        headers[key] = value
+
+    #routing 
+    if path == "/":
+        responce_body = f"{method} Home Page"
+    elif path == "/Hello":
+        responce_body = f"{method} Hello Marco"
+    else:
+        responce_body = f"{method} 404 not found"
+    
+    return method, path, responce_body, headers
+
+
+
+def handle_client(client):
+    #set up get request
+    data = client.recv(1024)
+    
+    method, path, response_body, headers = parce_request(data)
+
+    response = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: Text/plain\r\n"
+        f"content-Lenght: {len(response_body)}\r\n"
+        "\r\n"
+        f"{response_body}"
+    )
+    return response
+    
+
 
 #Start server
 server = socket.socket()
@@ -11,42 +56,8 @@ print("Server is listening...")
 client, address = server.accept()
 print("Client connected", address)
 
-#set up get request
-data = client.recv(1024)
-request = data.decode()
-
-lines, body = request.split("\r\n\r\n")
-
-lines = lines.split("\r\n")
-method, path, version = lines[0].split(" ")
-
-headers = {}
-for line in lines[1:]:
-    if line == " ":
-        break
-    key, value = line.split(":", 1)
-    headers[key] = value
-
-#routing 
-if path == "/":
-    responce_body = f"{method} Home Page"
-elif path == "/Hello":
-    responce_body = f"{method} Hello Marco"
-elif path == "/close":
-    responce_body = f"{method} Connection Closed BYEEEE"
-else:
-    responce_body = f"{method} 404 not found"
-
-
-response = (
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: Text/plain\r\n"
-    f"content-Lenght: {len(responce_body)}\r\n"
-    "\r\n"
-    f"{responce_body}"
-)
+response = handle_client(client)
 
 #Close connection
 client.sendall(response.encode())
 client.close()
-
